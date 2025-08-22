5da723a24374c9c45dca9cbb9c04149f5c10518a
Jhanela V <Jhanela V@DESKTOP-NKBF7UO>
2025-08-22 15:35:40 -0500

    carga inicial

--- src/examenparcial/ExamenParcial.java
+++ src/examenparcial/ExamenParcial.java
@@ -6,7 +6,7 @@ public class ExamenParcial {
     public static void main(String[] args) {
         //Declaración de variables
         String empleado;
-        double ht,th,sb,d,sn;
+        double ht,th,sb,d,sn,as;
         //Creando el objeto de lectura
         Scanner lectura=new Scanner(System.in);
         //Entrada de datos
@@ -20,9 +20,11 @@ public class ExamenParcial {
         sb=ht*th;
         d=sb*0.13;
         sn=sb-d;
+        as=sb*0.09;
         //Salida de datos
         System.out.println("Sueldo bruto: "+sb);
         System.out.println("Descuento: "+d);
         System.out.println("Sueldo neto: "+sn);
+        System.out.println("Aporte ESSALUD: "+as);
     }//Fin del método    
 }//Fin del programa
